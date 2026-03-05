522a34e0e7f81d587bca5f3bd3e56f73cb505ebb
Nadiia Vorotova <vorotova.nadya@gmail.com>
2026-03-05 19:25:41 +0200

    9 MobX

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,13 +1,19 @@
 import { Group } from "@mui/icons-material";
-import { AppBar, Box, Container, MenuItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Container, LinearProgress, MenuItem, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import { useStore } from "../../lib/hook/useStore";
+import { Observer } from "mobx-react-lite";
 
 
 export default function NavBar() {
+const {uiStore} = useStore();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ background: 'linear-gradient(135deg, #2234AB 0%, #45C1D1 69%, #B84AD9 89%)' }}>
+      <AppBar position="static" sx={{ background: 'linear-gradient(135deg, #2234AB 0%, #45C1D1 69%, #B84AD9 89%)',
+        position: 'relative'
+       }}>
         <Container maxWidth='xl'>
           <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box>
@@ -23,12 +29,30 @@ export default function NavBar() {
               <MenuItemLink to='/createActivity'>
                 Create Activity
               </MenuItemLink>
+              <MenuItemLink to='/counter'>
+                Counter
+              </MenuItemLink>
             </Box>
             <MenuItem>
               User menu
             </MenuItem>
           </Toolbar>
         </Container>
+
+        <Observer>
+          {() => uiStore.isLoading ? (
+            <LinearProgress 
+            color="secondary"
+            sx={{
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: 4
+            }}
+            />
+          ) : null}
+        </Observer>
 
       </AppBar>
     </Box>
